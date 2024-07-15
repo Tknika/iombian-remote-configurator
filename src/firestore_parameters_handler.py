e52e196c4d8da70c86fb14294c0cafe8c47214c8
Aitor Iturrioz <aiturrioz@tknika.eus>
2024-07-15 12:42:39 +0200

    Add a new upload_new_config method
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- src/firestore_parameters_handler.py
+++ src/firestore_parameters_handler.py
@@ -28,7 +28,8 @@ class FirestoreParametersHandler(FirestoreClientHandler):
     def stop(self):
         logger.debug("Stopping Firestore Parameters Handler")
         try:
-            self.client.collection(self.devices_path).document(self.device_id).update({"connected": False}, timeout=2)
+            self.client.collection(self.devices_path).document(
+                self.device_id).update({"connected": False}, timeout=2)
         except Exception as e:
             pass
         if self.device_subscription:
@@ -44,11 +45,14 @@ class FirestoreParametersHandler(FirestoreClientHandler):
     def on_client_initialized(self):
         logger.info("Firestore client initialized")
         self.devices_path = f"users/{self.user_id}/devices"
-        self.client.collection(self.devices_path).document(self.device_id).update({"last_connection": int(int(datetime.now().timestamp()))}, timeout=2)
-        self.client.collection(self.devices_path).document(self.device_id).update({"connected": True}, timeout=2)
+        self.client.collection(self.devices_path).document(self.device_id).update(
+            {"last_connection": int(int(datetime.now().timestamp()))}, timeout=2)
+        self.client.collection(self.devices_path).document(
+            self.device_id).update({"connected": True}, timeout=2)
         if self.device_subscription:
             return
-        self.device_subscription = self.client.collection(self.devices_path).document(self.device_id).on_snapshot(self._on_device_update)
+        self.device_subscription = self.client.collection(self.devices_path).document(
+            self.device_id).on_snapshot(self._on_device_update)
 
     def on_server_not_responding(self):
         logger.error("Firestore server not responding")
@@ -57,6 +61,24 @@ class FirestoreParametersHandler(FirestoreClientHandler):
     def on_token_expired(self):
         logger.debug("Refreshing Firebase client token id")
         threading.Timer(self.RESTART_DELAY_TIME_S, self.restart).start()
+
+    def upload_new_config(self, config):
+        config_date = config.get("config_date")
+        if not config_date:
+            config_date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+            config.update({"config_date": config_date})
+        self.initialize_client(notify=False)
+        if not self.client:
+            logger.debug(
+                "Firebase client not ready, cannot upload new configuration")
+            return
+        updated_field = {f"{self.KEYWORD}": config}
+        try:
+            self.client.collection(self.devices_path).document(
+                self.device_id).update(updated_field, timeout=2)
+        except Exception as e:
+            logger.error("Firebase client error, restart the handler")
+            threading.Timer(self.RESTART_DELAY_TIME_S, self.restart).start()
 
     def upload_config(self, config):
         config_date = config.get("config_date")
@@ -72,9 +94,11 @@ class FirestoreParametersHandler(FirestoreClientHandler):
         if not self.client:
             logger.debug("Firebase client not ready, cannot create device")
             return
-        initial_data = { "creation_date": datetime.now().strftime("%Y/%m/%dT%H:%M:%S"), "last_connection": 0, self.KEYWORD: {} }
+        initial_data = {"creation_date": datetime.now().strftime(
+            "%Y/%m/%dT%H:%M:%S"), "last_connection": 0, self.KEYWORD: {}}
         try:
-            self.client.collection(self.devices_path).add(initial_data, self.device_id, timeout=2)
+            self.client.collection(self.devices_path).add(
+                initial_data, self.device_id, timeout=2)
         except Exception as e:
             logger.error("Firebase client error, restart the handler")
             threading.Timer(self.RESTART_DELAY_TIME_S, self.restart).start()
@@ -82,11 +106,13 @@ class FirestoreParametersHandler(FirestoreClientHandler):
     def add_parameters(self, parameters_value, parameters_key):
         self.initialize_client(notify=False)
         if not self.client:
-            logger.debug("Firebase client not ready, add new parameters configuration")
+            logger.debug(
+                "Firebase client not ready, add new parameters configuration")
             return
-        updated_field = { f"{self.KEYWORD}.{parameters_key}" : parameters_value }
+        updated_field = {f"{self.KEYWORD}.{parameters_key}": parameters_value}
         try:
-            self.client.collection(self.devices_path).document(self.device_id).update(updated_field, timeout=2)
+            self.client.collection(self.devices_path).document(
+                self.device_id).update(updated_field, timeout=2)
         except Exception as e:
             logger.error("Firebase client error, restart the handler")
             threading.Timer(self.RESTART_DELAY_TIME_S, self.restart).start()
@@ -96,13 +122,11 @@ class FirestoreParametersHandler(FirestoreClientHandler):
             return
         device_info = document_snapshot[0].to_dict()
 
-        cloud_config = self._filter_last_configuration(document_snapshot)
+        cloud_config = self._filter_last_configuration(device_info)
 
-        threading.Thread(target=self.parameters_update_callback, args=[cloud_config]).start()
+        threading.Thread(target=self.parameters_update_callback,
+                         args=[cloud_config]).start()
 
-    def _filter_last_configuration(self, device_snapshot):
-        device_configurations = device_snapshot[-1].to_dict().get(self.KEYWORD, {})
-        if not device_configurations: return {}
-        last_configuration_key = sorted(device_configurations.keys())[-1]
-        last_configuration = device_configurations.get(last_configuration_key, {})
-        return last_configuration
+    def _filter_last_configuration(self, device_info):
+        device_configuration = device_info.get(self.KEYWORD, {})
+        return device_configuration
--- src/main.py
+++ src/main.py
@@ -6,14 +6,17 @@ import signal
 from communication_module import CommunicationModule
 from firestore_parameters_handler import FirestoreParametersHandler
 
-logging.basicConfig(format='%(asctime)s %(levelname)-8s - %(name)-16s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s - %(name)-16s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def stop():
     logger.info("Stopping IoMBian Remote Configurator Service")
-    if firestore_parameters_handler: firestore_parameters_handler.stop()
-    if comm_module: comm_module.stop()
+    if firestore_parameters_handler:
+        firestore_parameters_handler.stop()
+    if comm_module:
+        comm_module.stop()
 
 
 def signal_handler(sig, frame):
@@ -23,10 +26,11 @@ def signal_handler(sig, frame):
 def on_firestore_parameters_update(firestore_config):
     local_config = comm_module.execute_command("get_config")
     if not firestore_config:
-        logger.warning("No configuration available in the cloud, uploading the local one")
-        firestore_parameters_handler.upload_config(local_config)
+        logger.warning(
+            "No configuration available in the cloud, uploading the local one")
+        firestore_parameters_handler.upload_new_config(local_config)
         return
-    
+
     local_config_date = local_config.get("config_date", "2000-01-01T00:00:00")
     firestore_config_date = firestore_config.get("config_date")
 
@@ -35,13 +39,15 @@ def on_firestore_parameters_update(firestore_config):
         return
 
     if firestore_config_date == local_config_date:
-        logger.info(f"Both configs are equal, nothing has changed ({local_config_date})")
+        logger.info(
+            f"Both configs are equal, nothing has changed ({local_config_date})")
     elif firestore_config_date > local_config_date:
-        logger.info("There is a new configuration in the cloud, saving it locally")
+        logger.info(
+            "There is a new configuration in the cloud, saving it locally")
         comm_module.execute_command("save_config", firestore_config)
     elif firestore_config_date < local_config_date:
         logger.warning("The cloud config is old, uploading the local one")
-        firestore_parameters_handler.upload_config(local_config)    
+        firestore_parameters_handler.upload_config(local_config)
 
 
 if __name__ == "__main__":
@@ -57,7 +63,8 @@ if __name__ == "__main__":
     refresh_token = comm_module.execute_command("get_refresh_token")
     device_id = comm_module.execute_command("get_device_id")
 
-    firestore_parameters_handler = FirestoreParametersHandler(api_key, project_id, refresh_token, device_id, on_firestore_parameters_update)
+    firestore_parameters_handler = FirestoreParametersHandler(
+        api_key, project_id, refresh_token, device_id, on_firestore_parameters_update)
     firestore_parameters_handler.start()
 
     signal.signal(signal.SIGINT, signal_handler)
